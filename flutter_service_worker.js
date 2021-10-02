8c02356b53ad2eee45558f9c9f594e3befaa0dc4
Hesam <hesamnasiri42@gmail.com>
2021-10-02 21:36:40 +0330

    goh

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -71,9 +71,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "7bf5e90a314a758794bb52e4731a8650",
 "icons/Icon-192.png": "adda019cede461f645cd78489712a02a",
 "favicon.ico": "f13a0e0a83c1f6445126aabe600fabeb",
-"index.html": "e7d648a80ffe74f498bd9583b9b954ca",
-"/": "e7d648a80ffe74f498bd9583b9b954ca",
-"main.dart.js": "c150a5d50cd5a6ff243cfd954d312d25"
+"index.html": "22205130a41c416ec3f34c01f6fd47a6",
+"/": "22205130a41c416ec3f34c01f6fd47a6",
+"main.dart.js": "f8894361b152a031d4d92cddaca643f0"
 };
 
 // The application shell files that are downloaded before a service worker can
